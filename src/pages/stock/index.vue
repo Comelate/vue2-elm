dce99eca933123550907527598b8fa2ca940e0e0
蒋Sir <dongbelate@hotmail.com>
2019-07-01 17:27:25 +0800

    add stock page

---
+++ src/pages/stock/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>{{data}}</div>
+</template>
+
+<script>
+    export default {
+        name: 'stock',
+        data() {
+            return {
+                data: ''
+            }
+        },
+        methods: {
+            getStocks: function () {
+
+            }
+        },
+        created() {
+            this.getStocks();
+        }
+    }
+</script>
